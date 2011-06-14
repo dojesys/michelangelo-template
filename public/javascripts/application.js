2c44a6e97ef75e04f3e9981a64723a830a9b7eda
Johanan Pacheco <johananp@gmail.com>
2011-06-14 13:37:14 -0430

    Uso de Compass/Blueprint

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function() {
+	if ($("#flash:has(*)").length) {
+  	$('#flash').delay(500).fadeIn('normal', function() {
+    	$(this).delay(2500).fadeOut();
+  	});
+	}
+});
+
+$(function() {
+	$('#profile_birthday').datepicker( { 
+		dateFormat: 'dd/mm/yy', 
+		autoSize: true, 
+		showOn: "button",
+		showAnim: 'slide',
+		buttonImage: "/images/calendar.gif",
+		buttonImageOnly: true
+	});
+});	
